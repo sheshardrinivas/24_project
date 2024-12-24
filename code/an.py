14848d85a9b4892e16038d3dd5261c70f2341c89
sheshadrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2024-12-24 12:43:56 +0530

    updated code

--- code/an.py
+++ code/an.py
@@ -4,5 +4,8 @@ def string_print1(string_,time_,color_):
     for i in range(len(string_)):
         print(f"{color_}{string_[i]}",end="", flush=True)
         time.sleep(time_)
-    print(" ")
+def string_print2(string_,time_,color_):
 
+    for i in range(len(string_)):
+        print(f"{color_}{string_[i]}",end="", flush=True)
+        time.sleep(time_)
--- code/main.py
+++ code/main.py
@@ -5,7 +5,8 @@ from time import sleep
 import json
 import pyfiglet
 import progressbar
-from an import string_print1
+from an import string_print1,string_print2
+import getpass
 
 # List available fonts if needed
 # print(pyfiglet.getFonts())
@@ -14,7 +15,7 @@ from an import string_print1
 
 
 try:
- 
+
   print('\033c', end='')
   ascii_art = pyfiglet.figlet_format("BMI", font="starwars",)  # 'slant' is somewhat similar
   print(Fore.RED,ascii_art)
@@ -29,16 +30,22 @@ try:
   bar.finish()
   sleep(1)
   print('\033c', end='')
-  w=input("enter your weight in kgs -> ")
+  string_print1("enter your weight in kgs ->",0.02,Fore.BLUE)
+  w=input(" ")
+  print('\033c', end='')
   print(" ")
-  h=input("enter your height in meters -> ")
+  string_print1("enter your height in meters ->",0.02,Fore.BLUE)
+  h=input(" ")
   print(" ")
-  age=input("enter your age -> ")
+  string_print1("enter your age ->",0.02,Fore.BLUE)
+  age=input(" ")
   print(" ")
-  act=input("enter how active your are  -> ")
+  string_print1("enter how active your are  ->",0.02,Fore.BLUE)
+  act=input(" ")
   print(" ")
-  gen=input("Boy or Girl-> ")
-  print(" ")
+  string_print1("Boy or Girl->",0.02,Fore.BLUE)
+  gen=input(" ")
+  print(f"{Fore.RESET} ")
   age=float(age)
   act=str(act)
   w=float(w)
@@ -79,6 +86,7 @@ try:
   elif  bmi >40 :
       print("extermly obese")
       status="extermly obese"
+
   if act=="no":
       bmr=bmr*1.2
       act=act1
@@ -95,11 +103,11 @@ try:
       bmr=bmr*1.725
       act=act1
 
-
+  print("")
   print(f"bmr {bmr}")
 
   print(" ")
-  print("18 and less is under weight. 18 to 25 is normal, 25 to 30 is overweight, 30 to 40 is obese ,And over 40 extermly obese")
+  string_print1("18 and less is under weight. 18 to 25 is normal, 25 to 30 is overweight, 30 to 40 is obese ,And over 40 extermly obese",0.02,"")
   print(" ")
   print(" ")
   map=[[1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
@@ -123,135 +131,189 @@ try:
 
       for i in range(len(map)):
           for x in range(len(map[i])):
-              
+
               if map[i][x] ==0:
                 print("   ",end="")
-                
+
 
               if map[i][x] ==1:
-              
+
                 print(f"{Fore.WHITE} | ",end="")
 
 
               if map[i][x] ==2:
-              
+
                 print(f"{Fore.WHITE} . ",end="")
               if map[i][x] ==3:
-              
+
                 print(f"{Fore.LIGHTYELLOW_EX} - ",end="")
               if map[i][x] ==4:
-              
+
                 print(f"{Fore.GREEN} - ",end="")
               if map[i][x] ==5:
-              
+
                 print(f"{Fore.YELLOW} - ",end="")
               if map[i][x] ==6:
-              
+
                 print(f"{Fore.LIGHTRED_EX} - ",end="")
               if map[i][x] ==7:
-              
+
                 print(f"{Fore.RED} - ",end="")
 
-              
+
 
 
           print("")
           sleep(0.04)
+
+  def r1():
+      for y in range(len(map)):
+          for x in range(len(map[y])):
+
+              if map[y][x] ==0:
+                string_print2("   ",0.02,"")
+
+
+              if map[y][x] ==1:
+
+                string_print2(" | ",0.02,Fore.WHITE)
+
+
+              if map[y][x] ==2:
+
+
+                string_print2(" . ",0.02,Fore.WHITE)
+              if map[y][x] ==3:
+
+
+                string_print2(" - ",0.02,Fore.LIGHTYELLOW_EX)
+              if map[y][x] ==4:
+
+
+                string_print2(" - ",0.02,Fore.GREEN)
+              if map[y][x] ==5:
+
+
+                string_print2(" - ",0.02,Fore.YELLOW)
+              if map[y][x] ==6:
+
+
+                string_print2(" - ",0.02,Fore.LIGHTRED_EX)
+              if map[y][x] ==7:
+
+
+                string_print2(" - ",0.02,Fore.RED)
+          print("")
           
-          
 
 
-      
 
 
-          
-      
-    
-        
-      
-  render_map()
-  print("  ") 
-  print(" ",end=" ") 
+
+
+
+
+
+
+  #render_map()
+  r1()
+
+  print("  ")
+  print(" ",end=" ")
   print(f"{Fore.LIGHTYELLOW_EX}underweight",end=" ")
   print(f"{Fore.GREEN}normal",end=" ")
   print(f"{Fore.YELLOW}overweight",end=" ")
   print(f"{Fore.LIGHTRED_EX}obese",end=" ")
   print(f"{Fore.RED}extermly obese",end=" ")
 
-  print(" ") 
-  print("  ") 
-  print(" ",end=" ") 
+  print(" ")
+  print("  ")
+  print(" ",end=" ")
   print(f"{Fore.LIGHTYELLOW_EX}less than 18.5",end=",")
   print(f"{Fore.GREEN}18.5 to 25",end=",")
   print(f"{Fore.YELLOW}25 to 30",end=",")
   print(f"{Fore.LIGHTRED_EX}30 to 40",end=",")
   print(f"{Fore.RED}more than 40",end=" ")
 
-  print(" ") 
-  print(" ") 
+  print(" ")
+  print(" ")
   with open('code/test.json', 'r') as file:
               t1 = json.load(file)
   if status == "underweight":
 
       string_print1(f"{t1["treatment1"][0][0]}",0.02,Fore.LIGHTYELLOW_EX)
+      print("")
 
       string_print1(f"{t1["treatment1"][1][0]}",0.02,Fore.LIGHTYELLOW_EX)
+      print("")
 
       string_print1(f"{t1["treatment1"][2][0]}",0.02,Fore.LIGHTYELLOW_EX)
+      print("")
 
       string_print1(f"{t1["treatment1"][3][0]}",0.02,Fore.LIGHTYELLOW_EX)
+      print("")
 
-      
-    
+
+
   if status == "normal":
 
-      string_print1(f"{t1["treatment2"][0][0]}",0.08,Fore.GREEN)
-      
-      string_print1(f"{t1["treatment2"][1][0]}",0.08,Fore.GREEN)
-      
-      string_print1(f"{t1["treatment2"][2][0]}",0.08,Fore.GREEN)
-      
-      string_print1(f"{t1["treatment2"][3][0]}",0.08,Fore.GREEN)
-      
-      
+      string_print1(f"{t1["treatment2"][0][0]}",0.02,Fore.GREEN)
+      print("")
+
+      string_print1(f"{t1["treatment2"][1][0]}",0.02,Fore.GREEN)
+      print("")
+
+      string_print1(f"{t1["treatment2"][2][0]}",0.02,Fore.GREEN)
+      print("")
+
+      string_print1(f"{t1["treatment2"][3][0]}",0.02,Fore.GREEN)
+      print("")
+
+
   if status == "overweight":
 
-      string_print1(f"{t1["treatment3"][0][0]}",0.08,Fore.YELLOW)
-      string_print1(f"{t1["treatment3"][1][0]}",0.08,Fore.YELLOW)
-      string_print1(f"{t1["treatment3"][2][0]}",0.08,Fore.YELLOW)
-      string_print1(f"{t1["treatment3"][3][0]}",0.08,Fore.YELLOW)
- 
+      string_print1(f"{t1["treatment3"][0][0]}",0.02,Fore.YELLOW)
+      print("")
+      string_print1(f"{t1["treatment3"][1][0]}",0.02,Fore.YELLOW)
+      print("")
+      string_print1(f"{t1["treatment3"][2][0]}",0.02,Fore.YELLOW)
+      print("")
+      string_print1(f"{t1["treatment3"][3][0]}",0.02,Fore.YELLOW)
+      print("")
+
 
   if status == "obese":
 
-      string_print1(f"{t1["treatment4"][0][0]}",0.08,Fore.LIGHTRED_EX)
-      string_print1(f"{t1["treatment4"][1][0]}",0.08,Fore.LIGHTRED_EX)
-      string_print1(f"{t1["treatment4"][2][0]}",0.08,Fore.LIGHTRED_EX)
-      string_print1(f"{t1["treatment4"][3][0]}",0.08,Fore.LIGHTRED_EX)
+      string_print1(f"{t1["treatment4"][0][0]}",0.02,Fore.LIGHTRED_EX)
+      print("")
+      string_print1(f"{t1["treatment4"][1][0]}",0.02,Fore.LIGHTRED_EX)
+      print("")
+      string_print1(f"{t1["treatment4"][2][0]}",0.02,Fore.LIGHTRED_EX)
+      print("")
+      string_print1(f"{t1["treatment4"][3][0]}",0.02,Fore.LIGHTRED_EX)
+      print("")
 
   if status == "extermly obese":
 
-      string_print1(f"{t1["treatment5"][0][0]}",0.08,Fore.RED)
-      string_print1(f"{t1["treatment5"][1][0]}",0.08,Fore.RED)
-      string_print1(f"{t1["treatment5"][2][0]}",0.08,Fore.RED)
-      string_print1(f"{t1["treatment5"][3][0]}",0.08,Fore.RED)
-  print(" ")
+      string_print1(f"{t1["treatment5"][0][0]}",0.02,Fore.RED)
+      print("")
+      string_print1(f"{t1["treatment5"][1][0]}",0.02,Fore.RED)
+      print("")
+      string_print1(f"{t1["treatment5"][2][0]}",0.02,Fore.RED)
+      print("")
+      string_print1(f"{t1["treatment5"][3][0]}",0.02,Fore.RED)
+      print("")
 
 
-    
-  print(f"{Fore.WHITE}In your diet there should {round(carbs,2)}% Carbohydrates , {abs(round(V_and_M,2))}% vitments and minerals , {round(protein,2)}% Protein and {round(fats,2)}% Fats")
+
+  print("")
+  string_print1(f"In your diet there should {round(carbs,2)}% Carbohydrates , {abs(round(V_and_M,2))}% vitments and minerals , {round(protein,2)}% Protein and {round(fats,2)}% Fats",0.02,Fore.WHITE)
   print(""*50)
 except KeyboardInterrupt:
     print('\033c', end='')
-    
-    
 
 
-    ascii_art = pyfiglet.figlet_format("programme terminated", font="starwars",)  # 'slant' is somewhat similar
+
+
+    ascii_art = pyfiglet.figlet_format("programme terminated", font="slant",)  # 'slant' is somewhat similar
     print(ascii_art)
-  
-    
-   
-
-
-    
